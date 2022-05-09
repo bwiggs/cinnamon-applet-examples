858dc18c79fa03e06ad8e350b98ff795e1895755
Brian Wigginton <brianwigginton@gmail.com>
2022-05-08 23:33:42 -0500

    updates to simple

--- simple@bwiggs.com/README.md
+++ simple@bwiggs.com/README.md
@@ -1,3 +1,15 @@
 # Simple Applet
 
 The "hello world" cinnamon applet.
+
+## Applet types
+
+Your applet class can inherit from one of a few different Applet types.
+
+- `TextApplet` - Applet that shows text in the menubar.
+- `IconApplet` - Applet that shows an icon in the menubar.
+- `TextIconApplet` - Applet that shoes an icon and a text in the menubar. 
+
+# Resources
+
+- Cinnamon Applet Source Code - https://github.com/linuxmint/cinnamon/blob/master/js/ui/applet.js
--- simple@bwiggs.com/applet.js
+++ simple@bwiggs.com/applet.js
@@ -1,14 +1,16 @@
 const Applet = imports.ui.applet;
 
+var UUID;
+
 class MyApplet extends Applet.TextApplet {
     constructor(metadata, orientation, panel_height, instance_id) {
         super(orientation, panel_height, instance_id);
-        this.set_applet_label("hello world");
-        this.set_applet_tooltip("this is the tooltip");
-
+        this.set_applet_label(UUID);
+        this.set_applet_tooltip("hello world!");
     }
 }
 
 function main(metadata, orientation, panel_height, instance_id) {
+    UUID = metadata.uuid;
     return new MyApplet(metadata, orientation, panel_height, instance_id);
 }
